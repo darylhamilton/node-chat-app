18b9ebbecc84f3fe9a7b2297440122a7ebe4dc02
Daryl Hamilton <daryl.hamilton@outlook.com>
2018-11-27 21:11:18 +1300

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -14,6 +14,26 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newEmail', {
+    from: 'mike@example,com',
+    text: 'What is going on',
+    createdAt: 123
+  });
+
+  socket.on('createEmail', (newEmail) => {
+    console.log('createEmail', newEmail);
+  });
+
+  socket.on('createMessage', (newMessage) => {
+    console.log('newMessage', newMessage);
+  });
+
+  socket.emit('newMessage', {
+    from: 'daryl@example.com',
+    text: 'Hello there!',
+    createdAt: 1234
+  });
+
   socket.on('disconnect', () => {
     console.log('Disconnected from client');
   });
